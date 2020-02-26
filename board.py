f79298c70ffbce6cd2394f80ccc7658abd2ca233
Mark Tilton <mark.tilton.a@gmail.com>
2020-02-25 20:35:39 -0500

    Add game loop

--- board.py
+++ board.py
@@ -1,5 +1,11 @@
 import numpy as np
 
+char_map = {
+    0: ' ',
+    1: 'X',
+    2: 'O'
+}
+
 class Board:
 
     def __init__(self):
@@ -14,29 +20,53 @@ class Board:
 
     def make_move(self, action):
         new_board = self.clone()
+        # Treat cells as row-major to make repr easier
         new_board.cells[action.y, action.x] = action.val
         return new_board
 
 
+    def get_winner(self):
+        # Check rows / columns
+        for i in range(3):
+            val = self.cells[i, i]
+            if val == 0:
+                continue
+            if self.cells[(i + 1) % 3, i] == val and self.cells[(i + 2) % 3, i] == val:
+                return val
+            if self.cells[i, (i + 1) % 3] == val and self.cells[i, (i + 2) % 3] == val:
+                return val
+        # Check diagonals
+        center_val = self.cells[1, 1]
+        if center_val != 0:
+            if self.cells[0, 0] == center_val and self.cells[2, 2] == center_val:
+                return center_val
+            if self.cells[0, 2] == center_val and self.cells[2, 0] == center_val:
+                return center_val
+        return None
+
+
     @staticmethod
-    def repr_column(column):
-        char_map = {
-            0: ' ',
-            1: 'X',
-            2: 'O'
-        }
+    def get_player_name(player):
+        return char_map[player]
+
+
+    @staticmethod
+    def repr_row(row):
         result = ''
-        result += char_map[column[0]]
-        for row in column[1:]:
+        result += Board.get_player_name(row[0])
+        for cell in row[1:]:
             result += '|'
-            result += char_map[row]
+            result += Board.get_player_name(cell)
         return result
 
 
     def __repr__(self):
         result = ''
-        result += Board.repr_column(self.cells[0])
-        for column in self.cells[1:]:
-            result += '\n-----\n'
-            result += Board.repr_column(column)
+        result += '  0 1 2\n0 '
+        result += Board.repr_row(self.cells[0])
+        row_num = 1
+        for row in self.cells[1:]:
+            result += f'\n  -----\n{row_num} '
+            row_num += 1
+            result += Board.repr_row(row)
         return result
--- game.py
+++ game.py
@@ -5,10 +5,16 @@ board = Board()
 
 print(board)
 
+player = 0
 while True:
     move = input("Please enter your move: ")
     x = int(move[0])
     y = int(move[1])
-    val = 1
-    board = board.make_move(Action(x, y, val))
+    board = board.make_move(Action(x, y, player + 1))
     print(board)
+    winner = board.get_winner()
+    if winner != None:
+        print(f'{Board.get_player_name(winner)} wins!')
+        break
+    player = 1 - player
+
